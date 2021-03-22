e7e4bb5ade76e495a1568a88ea947dc463ab199b
Insoo <oasisholics@gmail.com>
2021-03-22 16:07:44 +0900

    add slider

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,50 +1,189 @@
 <template>
-  <div id="Welcome">
-    <img class="img-fluid " src="../assets/ttodong.jpg" width="400px" height="400">
-    <Nav></Nav>
-    <div class="col-sm banner">
-        <vue-carousel :data="BannerArray"></vue-carousel>
+    <div class="home"
+        id="home"
+        :class="[{'collapsed' : collapsed}, {'onmobile' : isOnMobile}]">
+        <div id="Welcome" :style=WelcomeStyle>
+            <img class="img-fluid " src="../assets/ttodong.jpg" width="400px" height="400">
+            
+            <div class="col-sm banner">
+                <vue-carousel :data="BannerArray"></vue-carousel>
+            </div>
+        </div>
+        <sidebar-menu 
+            :menu="menu"
+            :collapsed="collapsed"
+            :theme="selectedTheme"
+            :show-one-child="true"
+            @toggle-collapse="onToggleCollapse"
+            @item-click="onItemClick"/>
+        <div
+            v-if="isOnMobile && !collapsed"
+            class="sidebar-overlay"
+            @click="collapsed = true"
+        />
     </div>
-    <Footer></Footer>
-  </div>
 </template>
 
 <script>
-import Nav from "./Nav";
-import Footer from "./Footer"
 import VueCarousel from '@chenfengyuan/vue-carousel';
 
 
 export default {
   name: 'Welcome',
   components:{
-      Nav,
-      Footer,
       VueCarousel
   },
   data () {
     return {
-      BannerArray: Array
+        collapsed: false,
+        themes: [
+        {
+          name: 'Default theme',
+          input: ''
+        },
+        {
+          name: 'White theme',
+          input: 'white-theme'
+        }
+        ],
+        selectedTheme: 'dark-theme',
+        isOnMobile: false,
+        menu: [
+                    {
+                        header: true,
+                        title: 'Main Navigation',
+                        hiddenOnCollapse: true
+                    },
+                    {
+                        href: '/',
+                        title: 'HOME',
+                        icon: 'fas fa-home'
+                    },
+                    {
+                        href: '/pics',
+                        title: 'PICS',
+                        icon: 'fas fa-cut',
+                        child: [
+                            {
+                                href: '/pics/all',
+                                title: 'ALL'
+                            },
+                            {
+                                href: '/pics/short',
+                                title: 'SHORT'
+                            },
+                            {
+                                href: '/pics/midium',
+                                title: 'MIDIUM'
+                            },
+                            {
+                                href: '/pics/long',
+                                title: 'LONG'
+                            },
+                            {
+                                href: '/pics/max',
+                                title: 'LONG'
+                            }
+                        ]
+                    },
+                    {
+                        href: '/',
+                        title: 'RESERVATION',
+                        icon: 'fas fa-phone'
+                    },
+                    {
+                        href: '/',
+                        title: 'QnA',
+                        icon: 'fas fa-question'
+                    },
+                    {
+                        href: '/',
+                        title: 'About',
+                        icon: 'fas fa-cat'
+                    }
+                ],
+        BannerArray: Array,
+        WelcomeStyle:{
+            Width: '90%'
+        }
     }
   },
   created(){
-      this.BannerArray = new Array;
-      this.BannerArray.push('<img class="img-fluid " src="https://newsimg.hankookilbo.com/cms/articlerelease/2019/04/29/201904291390027161_3.jpg" width="900px" height="600px" />');
-      this.BannerArray.push('<img class="img-fluid " src="https://newsimg.hankookilbo.com/cms/articlerelease/2019/04/29/201904291390027161_3.jpg" width="900px" height="600px"/>');
-      this.BannerArray.push('<img class="img-fluid " src="https://newsimg.hankookilbo.com/cms/articlerelease/2019/04/29/201904291390027161_3.jpg" width="900px" height="600px"/>');
+    this.BannerArray = new Array;
+    this.BannerArray.push('<img class="img-fluid " src="https://newsimg.hankookilbo.com/cms/articlerelease/2019/04/29/201904291390027161_3.jpg" width="900px" height="600px" />');
+    this.BannerArray.push('<img class="img-fluid " src="https://newsimg.hankookilbo.com/cms/articlerelease/2019/04/29/201904291390027161_3.jpg" width="900px" height="600px"/>');
+    this.BannerArray.push('<img class="img-fluid " src="https://newsimg.hankookilbo.com/cms/articlerelease/2019/04/29/201904291390027161_3.jpg" width="900px" height="600px"/>');
     
+  },
+  mounted () {
+    this.onResize()
+    window.addEventListener('resize', this.onResize)
+  },
+  methods: {
+    onToggleCollapse (collapsed) {
+        console.log(collapsed)
+        this.collapsed = collapsed
+    },
+    onItemClick (event, item, node) {
+        console.log('onItemClick')
+        // console.log(event)
+        // console.log(item)
+        // console.log(node)
+    },
+    onResize () {
+        if (window.innerWidth <= 767) {
+        this.isOnMobile = true
+        this.collapsed = true
+        } else {
+        this.isOnMobile = false
+        this.collapsed = false
+        }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#Welcome{
+#home {
     text-align: center;
+    padding-left: 350px;
+    transition: 0.3s ease;
+}
+#home.collapsed {
+    padding-left: 50px;
+}
+#home.onmobile {
+    padding-left: 50px;
 }
 
-.banner{
-    margin-top:40px;
+.sidebar-overlay {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: #000;
+    opacity: 0.5;
+    z-index: 900;
+}
+
+.home {
+    padding: 50px;
+}
+
+.container {
+     max-width: 900px;
+}
+
+pre {
+    font-family: Consolas, monospace;
+    color: #000;
+    background: #fff;
+    border-radius: 2px;
+    padding: 15px;
+    line-height: 1.5;
+    overflow: auto;
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,19 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+Vue.use(VueSidebarMenu);
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
